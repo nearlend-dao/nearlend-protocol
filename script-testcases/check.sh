e55bcaa7e499eb21789990f30891e31ce97462c1
Lam NguyenSon <nguyensonlam@cryptopie-labo.com>
2022-08-26 17:58:15 +0700

    claim token to wallet user

--- script-testcases/check.sh
+++ script-testcases/check.sh
@@ -2,4 +2,7 @@
 export CONTRACT_ID=contract.main.burrow.near
 export NEAR_ENV=mainnet
 
+near view $CONTRACT_ID get_config '{}'
 near view $CONTRACT_ID get_assets_paged '{"from_index": 0, "limit": 10}'
+near view $CONTRACT_ID get_account '{"account_id": "near-lam.near"}' 
+
